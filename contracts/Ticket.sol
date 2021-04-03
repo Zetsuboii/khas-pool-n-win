1f930438e7727ce1ba0b392e17d15fc0a67479d9
Zetsuboii <hmzkrb@gmail.com>
2021-04-03 09:56:24 +0300

    change hard-coded msg.sender to parameter

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -4,45 +4,43 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Token is ERC721Enumerable {
-    constructor() ERC721("PoolTicket", "PNW") {}
+contract Ticket is ERC721Enumerable {
+  constructor() ERC721("PoolTicket", "PNW") {}
 
-    /**
+  /**
         Enumerable token to use in the pool
 
         @dev Get owner of a tokenId : ownerOf() 
         @dev Get total amount of tokens: totalSupply();
     */
 
-    /**
+  /**
         @dev Create a new token
     */
-    function createToken() internal {
-        // LIFO structure (?)
-        uint256 tokenId = totalSupply();
-        _safeMint(msg.sender, tokenId);
-    }
+  function createToken(address _owner) internal {
+    // LIFO structure (?)
+    uint256 tokenId = totalSupply();
+    _safeMint(_owner, tokenId);
+  }
 
-    /**
+  /**
         @dev Delete the first token of a given user
     */
-    function removeFirstToken(address _owner) internal {
-        uint256 tokenId = tokenOfOwnerByIndex(_owner, 0);
-        _burn(tokenId);
-    }
+  function removeFirstToken(address _owner) internal {
+    uint256 tokenId = tokenOfOwnerByIndex(_owner, 0);
+    _burn(tokenId);
+  }
 
-    /**
+  /**
         @dev Pick a random ticket from all tickets and return the owner
     */
-    function getRandomTicketOwner() internal view returns (address) {
-        uint256 totalTickets = totalSupply();
-        uint256 winnerTicket =
-            uint256(
-                keccak256(
-                    abi.encodePacked(block.timestamp, msg.sender, totalTickets)
-                )
-            ) % totalTickets;
-        uint256 ticketID = tokenByIndex(winnerTicket);
-        return ownerOf(ticketID);
-    }
+  function getRandomTicketOwner() internal view returns (address) {
+    uint256 totalTickets = totalSupply();
+    uint256 winnerTicket =
+      uint256(
+        keccak256(abi.encodePacked(block.timestamp, msg.sender, totalTickets))
+      ) % totalTickets;
+    uint256 ticketID = tokenByIndex(winnerTicket);
+    return ownerOf(ticketID);
+  }
 }
