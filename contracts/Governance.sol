41d3b1025ff7704450170f3c98eeba804f8b7d06
Zetsuboii <hmzkrb@gmail.com>
2021-04-18 15:40:34 +0300

    Token address contructor and set adress function

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Token.sol";
 
-contract Governance {
+contract Governance is Ownable {
     using EnumerableSet for EnumerableSet.UintSet;
 
     IERC20 public token;
 
-    constructor() {
-        token = new Token();
+    constructor(address _tokenAddress) {
+        token = Token(_tokenAddress);
     }
 
     enum Status {Created, Accepted, Rejected}
@@ -24,7 +25,12 @@ contract Governance {
     EnumerableSet.UintSet internal proposalIds;
     mapping(uint256 => Proposal) proposals;
 
+    // Proposals getter
     function getAllProposals() external view returns (Proposal[] memory) {}
+
+    function setTokenAdress(address _newAddress) external onlyOwner {
+        token = Token(_newAddress);
+    }
 
     function makeProposal(uint256 _proposalUri) public {}
 }
