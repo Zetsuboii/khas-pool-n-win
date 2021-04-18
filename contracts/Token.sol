8707555fddc68539db548c4fb282822f494fc809
Zetsuboii <hmzkrb@gmail.com>
2021-04-18 14:57:53 +0300

    Governance token

---
+++ contracts/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+/**
+    @title Pool N Win Governance Token
+    @author Hamza Karabağ
+
+    @dev Governance Token with limited supply
+ */
+contract Token is ERC20Capped {
+    constructor(uint256 _maxSupply)
+        ERC20Capped(_maxSupply)
+        ERC20("PoolGovernance", "PNWG")
+    {
+        require(_maxSupply > 0, "Max supply can't be 0");
+    }
+}
